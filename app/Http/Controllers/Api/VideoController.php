6310601213d74e22af4d71ddf75589123ffe7f82
Pedro Rodrigues <pir.pedro@gmail.com>
2020-09-16 16:10:41 -0300

    Include VideoController tests.

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -10,14 +10,25 @@ class VideoController extends BasicCrudController
     private $rules;
 
     public function __construct(){
-
+        $this->rules = [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'year_launched' => 'required|date_format:Y',
+            'opened' => 'boolean',
+            'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
+            'duration' => 'required|integer',
+        ];
     }
 
     protected function model(){
         return Video::class;
     }
 
-    protected function rulesStore(){}
+    protected function rulesStore(){
+        return $this->rules;
+    }
 
-    protected function rulesUpdate(){}
+    protected function rulesUpdate(){
+        return $this->rules;
+    }
 }
