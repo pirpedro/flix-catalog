145ece9eafafef3c1e186c475274d307b64826a2
Pedro Rodrigues <pir.pedro@gmail.com>
2020-09-28 21:18:02 -0300

    Change laravel-cors library.

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -39,7 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            HandleCors::class,
             'throttle:60,1',
             'bindings',
         ],
